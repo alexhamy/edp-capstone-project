abadc18385da4c468e3a4a1305c1c3a63c8b5128
evelynflandau <evelynflandau@gmail.com>
2025-07-10 16:21:10 -0400

    clothing cards and homepage

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import NavigationHeader from './components/navheader';
 import HomePage from './components/HomePage';
-function App() {
-  
-
+function App(props) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      await fetch("http://localhost:5000/api").then((response) =>{
+        if(response.status !== 200){
+          throw new Error("Error fetching data!")
+        }
+        return response.json()
+      }
+      ).then((responseData) => {
+        setData(responseData);
+      })
+    }
+    getData();
+  }, [])
   return (
+    
     <div style={{display:"flex", width:'100%', flexDirection:"column"}}>
       
       <Router>
         <NavigationHeader></NavigationHeader>
         <Routes>
-        <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<HomePage data={data}/>}/>
         </Routes>
       </Router>
     </div>
--- react/src/components/HomePage.jsx
+++ react/src/components/HomePage.jsx
@@ -1,9 +1,22 @@
+import ClothingCard from "./ClothingCard";
 
-export default function HomePage() {
+export default function HomePage(props) {
+    const clothes = props.data
+    const featured = clothes.filter((cloth)=>cloth.Rating === 5.0)
+    const selectedClothes = []
+    if(featured.length >= 4){
+        while(selectedClothes.length < 4){
+            let num = Math.floor(Math.random() * featured.length);
+            if(selectedClothes.indexOf(num) === -1) selectedClothes.push(num);
+        };
+    }
     return(
         <div style={{display:"flex", flexDirection:"column"}}>
             <h2>Welcome to Cool Clothes!</h2>
             <h3>Check out our featured items!</h3>
+            <div style={{display:"flex", flexDirection:"row"}}>
+                {selectedClothes.map((selected) => <ClothingCard item={featured[selected]} key={selected}/>)}
+            </div>
         </div>
     )
 }
