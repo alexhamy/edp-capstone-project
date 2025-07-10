b6438291ea4f514257446f71ae9ba72b1019adf6
evelynflandau <evelynflandau@gmail.com>
2025-07-10 14:10:21 -0400

    nav header

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import NavigationHeader from './components/navheader';
 function App() {
   
 
   return (
-    <>
-    
+    <div style={{display:"flex", width:'100%'}}>
+      
       <Router>
-
+      <NavigationHeader></NavigationHeader>
       </Router>
-    </>
+    </div>
   )
 }
 
