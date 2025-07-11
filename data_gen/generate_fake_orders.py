ff1dde0b911da9bf37589b092b9ed40274f1033a
Alice Smith <alice@webage.com>
2025-07-11 09:37:11 -0400

    updated datagen for nearestneighbor

--- data_gen/generate_fake_orders.py
+++ data_gen/generate_fake_orders.py
@@ -1,18 +1,15 @@
 import random
 import json
-from generate_item import gen_item
+from generate_item import gen_items
 
 DATA_SIZE = 5
 ORDER_SIZE = 5
 
+
 orders = []
 
-for i in range(DATA_SIZE):
-    order = []
-
-    for i in range(random.randint(1, ORDER_SIZE)):
-        order.append(gen_item())
-
+for _ in range(DATA_SIZE):
+    order = gen_items(random.randint(1, ORDER_SIZE))
     orders.append(order)
 
 with open("fake_orders.json", "w", encoding='utf-8') as f:
