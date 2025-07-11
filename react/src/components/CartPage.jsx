8a6c88e8f2f311af48b282e44363fe2df1c47d49
Alexander Hamilton <alexander0hamilton@gmail.com>
2025-07-11 11:44:22 -0400

    added order summary

--- react/src/components/CartPage.jsx
+++ react/src/components/CartPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ClothingCard from "./ClothingCard";
 export default function CartPage(props){
     console.log(props.cart)
@@ -12,11 +13,18 @@ export default function CartPage(props){
         }
         setTotal(sum)
     },[props])
+
+    let navigate = useNavigate(); 
+        const routeChange = () =>{ 
+            let path = `/checkout`; 
+            navigate(path);
+        }
+        
     return(
         <div>
             <h2>Items in cart:</h2>
             <h2>Total: ${total}</h2>
-            <button style={{width: '100%'}}>Checkout</button>
+            <button style={{width: '100%'}} onClick={routeChange}>Checkout</button>
             <div style={{display:'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridTemplateRows:'repeat(10, 1fr)', gridColumnGap:'0px', gridRowGap:'0px'}}>
                 {props.cart?.map((selected) => {return (<ClothingCard item={selected} key={selected.id}/>)})}
             </div>
